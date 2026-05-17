5f6692d1f810f7ff168035b8cc2b6ca87169bef2
ikhya1207 <ikhya.127@gmail.com>
2026-05-18 02:23:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function pesan(){
-    alert("Selamat datang di website saya!");
+body{
+    font-family: Arial;
+    margin:0;
+    background:#111;
+    color:white;
+}
+
+header{
+    background:#222;
+    padding:20px;
+    display:flex;
+    justify-content:space-between;
+}
+
+nav a{
+    color:white;
+    margin:10px;
+    text-decoration:none;
+}
+
+section{
+    padding:60px;
+    text-align:center;
+}
+
+.card{
+    background:#333;
+    padding:20px;
+    margin:15px;
+    border-radius:10px;
 }
